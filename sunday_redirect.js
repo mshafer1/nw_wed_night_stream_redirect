47ecb8af677b6597e881ae7c4a8fb5c81e25ea9a
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-06-03 12:22:17 -0500

    setup for sunday urls

--- sunday_redirect.js
+++ sunday_redirect.js
@@ -1,5 +1,5 @@
-var expiration_date = new Date(2021, 4, 31)  // the date that the urls here end
-var version = "1.0.7"
+var expiration_date = new Date(2021, 8, 4)  // the date that the urls here end
+var version = "1.0.8"
 var d = new Date(); // get the current date
 
 page_data = {
@@ -9,19 +9,20 @@ page_data = {
 
 
 
-// 2021 Q1
-// page_data[new Date(2021, 2, 10)] = 'https://youtu.be/ZmKth32nDt0';
-// page_data[new Date(2021, 2, 17)] = 'https://youtu.be/LK8hnVGddKY';
-// page_data[new Date(2021, 2, 24)] = 'https://youtu.be/TYuDBublCgw';
-// page_data[new Date(2021, 2, 31)] = "https://youtu.be/zFq0z1k5a18";
-// page_data[new Date(2021, 3, 7)] = "https://youtu.be/Z2Y5zEN-kFw";
-// page_data[new Date(2021, 3, 14)] = "https://youtu.be/B7U293FyIO0";
-// page_data[new Date(2021, 3, 21)] = "https://youtu.be/NfaISb55iFs";
-// page_data[new Date(2021, 3, 28)] = "https://youtu.be/2GGznnoDIjc";
-// page_data[new Date(2021, 4, 05)] = "https://youtu.be/K5yGy4BOxDw"
-// page_data[new Date(2021, 4, 12)] = "https://youtu.be/6eTeYKuYtQo"
-// page_data[new Date(2021, 4, 19)] = "https://youtu.be/GTPdKHE_nQU"
-// page_data[new Date(2021, 4, 26)] = "https://youtu.be/H9wLdbGVvm4"
+// 2021 Q2
+page_data[new Date(2021, 5, 6)] = 'https://youtu.be/ZTAKNPZOiIA'
+page_data[new Date(2021, 5, 13)] = 'https://youtu.be/2mN9XhulpQg'
+page_data[new Date(2021, 5, 20)] = 'https://youtu.be/UwI3vj7fnNQ'
+page_data[new Date(2021, 5, 27)] = 'https://youtu.be/TDZ99JO6pik'
+page_data[new Date(2021, 6, 4)] = 'https://youtu.be/loS9wsS_BWM'
+page_data[new Date(2021, 6, 11)] = 'https://youtu.be/j0fU_26iCzQ'
+page_data[new Date(2021, 6, 18)] = 'https://youtu.be/_dWhDtgd4vU'
+page_data[new Date(2021, 6, 25)] = 'https://youtu.be/W5VgsL2JJZk'
+page_data[new Date(2021, 7, 1)] = 'https://youtu.be/jUOzFU-godc'
+page_data[new Date(2021, 7, 8)] = 'https://youtu.be/w7TZIG14Cas'
+page_data[new Date(2021, 7, 15)] = 'https://youtu.be/bBChlFpciCw'
+page_data[new Date(2021, 7, 22)] = 'https://youtu.be/2ncUMo3f1fs'
+page_data[new Date(2021, 7, 29)] = 'https://youtu.be/u5d3hEwL3ms'
 
 
 function get_stream_link(d) {
@@ -90,7 +91,7 @@ function redirection() {
 }
 
 console.log(`Redirect link version: ${version}`)
-
+redirection()
 
 try {
 	// from https://stackoverflow.com/a/11279639
